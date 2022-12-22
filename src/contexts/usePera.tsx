518bd5b84e1d9729831faadc14dbca4a609281e0
unknown <luciano@koibanx.com>
2022-12-22 10:50:50 -0300

    add pera wallet connection

---
+++ src/contexts/usePera.tsx
@@ -0,0 +1,50 @@
+import React, { createContext, useContext, useEffect, useState } from 'react'
+import { PeraWalletConnect } from '@perawallet/connect';
+
+
+const PeraContext = createContext({})
+
+type Props = {
+    children: JSX.Element | JSX.Element[];
+};
+
+export const PeraProvider: React.FC<Props> = ({ children }: Props): JSX.Element => {
+  const peraWallet = new PeraWalletConnect();
+  const [accountAddress, setAccountAddress] = useState([])
+
+
+  const handleDisconnectWalletClick = () => {
+    peraWallet.disconnect();
+    setAccountAddress([]);
+
+  }
+
+  const handleConnectWalletClick = () =>{
+    peraWallet
+      .connect()
+      .then((newAccounts) => {
+        peraWallet.connector?.on("disconnect", handleDisconnectWalletClick);
+        setAccountAddress(newAccounts[0] as unknown as never[]);
+      }, (error) => {
+        if (error?.data?.type !== "CONNECT_MODAL_CLOSED") {
+          console.log(error)
+        }
+      });
+  }
+
+  useEffect(() => {
+    peraWallet.reconnectSession().then((accounts) => {
+      peraWallet.connector?.on("disconnect", handleDisconnectWalletClick);
+      if (accounts.length) setAccountAddress(accountAddress[0])
+    });
+  }, []);
+
+
+  return (
+    <PeraContext.Provider value={{ accountAddress, handleConnectWalletClick, handleDisconnectWalletClick}}>
+    {children}
+    </PeraContext.Provider>
+  )
+}
+
+export const usePera = (): any => useContext(PeraContext)
